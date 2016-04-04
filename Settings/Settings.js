a08c2125e8d3241bcfdbc19434d7f0c34c793649
CHANDRASEKHAR KAMATIGAM <kcsekhar13@gmail.com>
2016-04-04 23:35:08 +0200

    Added Quoted screen

--- Settings/Settings.js
+++ Settings/Settings.js
@@ -12,15 +12,22 @@ var Observable = require("FuseJS/Observable");
 	                {setting : "About" }
 	            ];
 		var currentPageTitle = Observable();
+
 		var secretAnswer = Observable("AaHooo!");
-		// console.log(settings[1]);
 
 		// test settings
 		var testValue = Observable("testValue");
+
 		var switchValue = Observable(false);
+
+		var quotesEnabled = Observable(false);
 
 		function passcodeSwitchChanged(arg){
 			console.log("passcodeSwitchChanged");
+		}
+
+		function quotesSwitchChanged(arg) {
+			console.log("Quotes Switch Changed");
 		}
 
 		var selectedOption = Observable("1");
@@ -34,6 +41,29 @@ var Observable = require("FuseJS/Observable");
 			{ option: "What is your favourite food?", code: "6"}
 		);
 
+		var defaultQuotes = Observable(
+			{quote:"In the midst of winter, i found there was within me , an invincible summer",code:"1"},
+			{quote:"MyQuotes1",code:"2"},
+			{quote:"MyQuotes2",code:"3"},
+			{quote:"MyQuotes3",code:"4"},
+			{quote:"MyQuotes4",code:"5"},
+			{quote:"MyQuotes", code:"6"},
+			{quote:"MyQuotes1",code:"7"},
+			{quote:"MyQuotes2",code:"8"},
+			{quote:"MyQuotes3",code:"9"},
+			{quote:"MyQuotes4",code:"10"},
+      {quote:"MyQuotes5",code:"11"},
+      {quote:"MyQuotes6",code:"12"},
+      {quote:"MyQuotes7",code:"13"},
+      {quote:"MyQuotes8",code:"14"},
+      {quote:"MyQuotes9",code:"15"}
+		);
+		var newQuote = Observable();
+
+		function addNewQuote(arg) {
+			console.log("Add New quote");
+		}
+
 		var selected = Observable();
 
 		function selectMe(arg){
@@ -45,9 +75,6 @@ module.exports =  {
 		settings: settings,
     currentPageTitle :"Settings",
 		passcodeSwitchChanged : passcodeSwitchChanged,
-		switchChanged: function (args) {
-                console.log("Switch value is: " + args.value);
-            },
 		dropdownOptions: dropdownOptions,
 		selected: selected,
 		selectMe: selectMe,
@@ -55,4 +82,9 @@ module.exports =  {
 		secretAnswer:secretAnswer,
 		testValue:testValue,
 		switchValue: switchValue,
+		quotesEnabled:quotesEnabled,
+		quotesSwitchChanged:quotesSwitchChanged,
+		defaultQuotes:defaultQuotes,
+		newQuote:newQuote,
+		addNewQuote : addNewQuote
 };
