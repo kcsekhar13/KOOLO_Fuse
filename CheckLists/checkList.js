d470ada4dea370cdd44e233422b53fd515254cc9
chandrasekhar <kcsekhar13@gmail.com>
2016-04-12 14:02:45 +0200

    added My Health Checklist screen

--- CheckLists/checkList.js
+++ CheckLists/checkList.js
@@ -1,5 +1,8 @@
 var Observable = require('FuseJS/Observable');
 
+var selected = Observable();
+var newCheckListText = Observable();
+
 var threeSentenses = {
   first :Observable(""),
   second:Observable(""),
@@ -18,10 +21,36 @@ function InitPage() {
 
 }
 
+function selectMyHealth() {
+  selected.value = "My Health";
+  newCheckListText.value ="New Goal";
+}
+
+function selectTransition() {
+  selected.value = "Ready for transition";
+  newCheckListText.value ="Transition";
+}
+
+function InitMyHealthPage() {
+  selectMyHealth();
+}
+
+function InitTransitionPage() {
+  selectTransition();
+}
+
+function print(arg) {
+  console.log( " Print" +JSON.stingify(arg));
+}
+
 module.exports={
+  save:saveThreeSentenses,
+  readThreeSentenses:readThreeSentenses,
+  selected:selected,
+  newCheckListText:newCheckListText,
+  InitMyHealthPage:InitMyHealthPage,
+  InitTransitionPage:InitTransitionPage,
   first:threeSentenses.first,
   second:threeSentenses.second,
-  thirs:threeSentenses.third,
-  save:saveThreeSentenses,
-  readThreeSentenses:readThreeSentenses
+  third:threeSentenses.third,
 }
