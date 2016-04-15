2d12f52a5bec8fbf15211b3660d3c68dae4d088c
chandrasekhar <kcsekhar13@gmail.com>
2016-04-15 21:30:20 +0200

    calendar swipe implementation

--- calendar.js
+++ calendar.js
@@ -43,6 +43,27 @@ function initPage() {
   console.log("validDays count " +validDays.length );
 }
 
+function initCalendar(date) {
+  validDays = [];
+  var d = new Date(date);
+  todayDate.value = d.getDate();
+  currentDate.value = d.toDateString();
+  currentMonth.value = d.getMonth()+1;
+  currentYear.value = d.getFullYear();
+  currentDayInWeek.value= d.getDay();
+  totalDaysInMonth.value = daysInMonth(currentMonth.value,currentYear.value);
+
+  for (var i = 0; i < currentDayInWeek.value; i++) {
+    validDays.push(new myDay('',false) );
+  }
+  for (var i = 1; i <= totalDaysInMonth.value; i++) {
+    validDays.push(new myDay(i,false));
+  }
+  console.log("Year  " +   currentYear.value + " Month :" + currentMonth.value);
+  console.log("Total days in current month " + totalDaysInMonth.value);
+  console.log("validDays count " +validDays.length );
+}
+
 function onDateSelected(date) {
   console.log(JSON.stringify(date));
   selectedDate.value = date.data.date;
@@ -52,6 +73,27 @@ function daysInMonth(month,year) {
     return new Date(year, month, 0).getDate();
 }
 
+function showNextMonth() {
+    console.log("Displaying next month " + currentMonth.value);
+    if(currentMonth.value == 11){
+      currentYear.value++;
+      currentMonth.value = 1;
+    }
+    var newDate = new Date(currentYear.value,currentMonth.value,1)
+    console.log("new date : " + newDate);
+    initCalendar(newDate);
+}
+
+function showPreviousMonth() {
+  console.log("Displaying Previous month ");
+  if(currentMonth.value == 11){
+    currentYear.value--;
+    currentMonth.value = 11
+  }
+  var newDate = new Date(currentYear.value,currentMonth.value,1)
+  console.log("new date : " + newDate);
+  initCalendar(newDate);
+}
 initPage();
 
 module.exports = {
@@ -61,5 +103,6 @@ module.exports = {
   totalDaysInMonth:totalDaysInMonth,
   validDays:validDays,
   onDateSelected:onDateSelected,
-  selectedDate:selectedDate
+  selectedDate:selectedDate,
+  showNextMonth:showNextMonth
 };
